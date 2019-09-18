696a2d38c612eb86fdafc54219c466b8496542e2
Lucian Cooper <cooperlucian@gmail.com>
2019-09-18 14:48:58 -0400

    build: added morgan & cors
     - added morgan & cors middleware dependencies.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express'),
     bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    cors = require('cors'),
     app = express();
 
 // configure env
@@ -8,6 +10,12 @@ require('dotenv').config();
 // setup body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// setup logging
+app.use(morgan('dev'));
+
+// setup cors
+app.use(cors());
 
 // ports
 const PORT = process.env.PORT || 3000;
